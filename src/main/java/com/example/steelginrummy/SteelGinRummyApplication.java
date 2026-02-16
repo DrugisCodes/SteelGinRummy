a604b54bc41b0f89c3c122432881d6912ab51a81
DrugisCodes <sigurd.staloy@gmail.com>
2026-02-16 18:29:59 +0100

    Initial commit

---
+++ src/main/java/com/example/steelginrummy/SteelGinRummyApplication.java
@@ -0,0 +1,13 @@
+package com.example.steelginrummy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteelGinRummyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SteelGinRummyApplication.class, args);
+	}
+
+}
